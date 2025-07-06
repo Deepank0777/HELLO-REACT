969f499fa7cbc0f42ede7ed8f3b7c5ab70b25b26
deepank0777 <kumar.best0777@gmail.com>
2025-07-06 18:26:53 +0530

    use state in the class based component

--- src/components/UserClassBasedComponent.js
+++ src/components/UserClassBasedComponent.js
@@ -4,10 +4,28 @@ class UserClassBasedComponent extends React.Component {
   constructor(props) {
     super(props);
     console.log("user class based component props", props);
+
+    this.state = {
+      count: 0,
+      count2: 0,
+      count3: 0,
+    };
   }
   render() {
+    const { count, count2 } = this.state;
     return (
       <div className="user-card">
+        <h1>Count: {count}</h1>
+        <button
+          onClick={() => {
+            this.setState({
+              count: count + 1,
+              count2: count2 + 1,
+            });
+          }}
+        >
+          Increment Count
+        </button>
         <h6>Rendering From Class Based Component </h6>
         <h2>Name: {this.props.name}</h2>
         <h3>Location: Delhi</h3>
--- src/components/UserFunctionBasedComponent.js
+++ src/components/UserFunctionBasedComponent.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
+
 const User = (props) => {
+  const [count] = useState(0);
   return (
     <div className="user-card">
+      <h1>Count: {count}</h1>
       <h6>Rendering From Function Based Compo </h6>
       <h2>Name: {props.name}</h2>
       <h3>Location: Delhi</h3>
